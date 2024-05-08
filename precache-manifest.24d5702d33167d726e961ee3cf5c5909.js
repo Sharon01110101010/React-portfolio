420fb711b4b6ef9a4de08b71f1d1915b88c5e5d6
Sharon Wong <swong21@earlham.edu>
2024-05-08 12:40:36 -0400

    Updates

---
+++ precache-manifest.24d5702d33167d726e961ee3cf5c5909.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f890602773854d8701205120843ebe7d",
+    "url": "/React-portfolio/index.html"
+  },
+  {
+    "revision": "5ed3d18a19b41a05e27c",
+    "url": "/React-portfolio/static/css/main.5cb4c223.chunk.css"
+  },
+  {
+    "revision": "8d278b0a5bb081e4f561",
+    "url": "/React-portfolio/static/js/2.a79ead11.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/React-portfolio/static/js/2.a79ead11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ed3d18a19b41a05e27c",
+    "url": "/React-portfolio/static/js/main.b85dd663.chunk.js"
+  },
+  {
+    "revision": "1b5359aa1094951a9f44",
+    "url": "/React-portfolio/static/js/runtime-main.f4a52cab.js"
+  },
+  {
+    "revision": "572815362e3a9ed170a2b99e1f371b43",
+    "url": "/React-portfolio/static/media/APT.57281536.pdf"
+  },
+  {
+    "revision": "c0a7466d2216cd6e41d6865ca7ee9d8f",
+    "url": "/React-portfolio/static/media/AWA.c0a7466d.pdf"
+  },
+  {
+    "revision": "4bc763a4154c19a7e5847eb94416d302",
+    "url": "/React-portfolio/static/media/Stark.4bc763a4.pdf"
+  },
+  {
+    "revision": "2a78c4c7f33d3aa2b24f6d0efadd3044",
+    "url": "/React-portfolio/static/media/about-me.2a78c4c7.jpg"
+  },
+  {
+    "revision": "5bf2c5e61416d7ab91f4679ba1eab6d5",
+    "url": "/React-portfolio/static/media/cv.5bf2c5e6.pdf"
+  },
+  {
+    "revision": "a58b70a5149bd97cae574384d1a68969",
+    "url": "/React-portfolio/static/media/me.a58b70a5.jpg"
+  },
+  {
+    "revision": "61cf9d00aa03208003fb9385ce9e20ac",
+    "url": "/React-portfolio/static/media/p1.61cf9d00.png"
+  },
+  {
+    "revision": "22a6ac7106bc84fcb2848cb107999148",
+    "url": "/React-portfolio/static/media/p2.22a6ac71.jpg"
+  },
+  {
+    "revision": "b7ba5698edd67583c7af7e01828e18dc",
+    "url": "/React-portfolio/static/media/p3.b7ba5698.png"
+  },
+  {
+    "revision": "39623438c8d51aee6c3e9e435955bd12",
+    "url": "/React-portfolio/static/media/t1.39623438.jpg"
+  },
+  {
+    "revision": "c0566c14947935ade7269a7f2084b7f8",
+    "url": "/React-portfolio/static/media/t2.c0566c14.jpg"
+  },
+  {
+    "revision": "9572cfa2d9a74d3183da6f491ab24edd",
+    "url": "/React-portfolio/static/media/t3.9572cfa2.jpg"
+  },
+  {
+    "revision": "aff8babb02a5e6cb0b3180c80d12bc79",
+    "url": "/React-portfolio/static/media/t4.aff8babb.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-portfolio/precache-manifest.c3cac927e1a70cd4098c584a4b04c56a.js"
+  "/React-portfolio/precache-manifest.24d5702d33167d726e961ee3cf5c5909.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/React-portfolio/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
